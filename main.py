70fafc7f727a723bbfc1c982e4d451afbb25944d
solar-shy <solar-shy@yandex.ru>
2020-10-13 19:03:41 +1000

    game

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+
+num = random.randint(0, 100)
+print(num)
+
+W = 480
+H = 360
+SILVER = (192, 192, 192)
+BLACK = (0, 0, 0)
+
+pygame.init()
+pygame.display.set_caption('Угадай число')
+pygame.mouse.set_visible(False)
+screen = pygame.display.set_mode((W, H))
+
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            run = False
