8633f71adc3765e1559d711171e7c0f0d48f4314
solar-shy <solar-shy@yandex.ru>
2020-10-20 19:09:08 +1000

    sus

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ W = 480
 H = 360
 SILVER = (192, 192, 192)
 BLACK = (0, 0, 0)
+numeral = ''
+move = 1
+block = 0
+start = 1
+OUTSIZE_BG = (0, -100)
 
 pygame.init()
 pygame.display.set_caption('Я по твоему тупой?')
-pygame.mouse.set_visible(False) 
+pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((W, H))
 
 bg = pygame.image.load('Image/room.png')
@@ -24,6 +29,25 @@ owl = pygame.image.load('Image/owl.png')
 owl_rect = owl.get_rect(center=(210, 120))
 dialog = pygame.image.load('Image/dialog.png')
 dialog_rect = dialog.get_rect()
+dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
+dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
+dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+
+# Добавление шрифта
+font = pygame.font.SysFont('Arial', 28, True, False)
+font2 = pygame.font.SysFont('Arial', 14, False, True)
+font_box = pygame.Surface((W - 30, font.get_height()))
+font_box_rect = font_box.get_rect(center=(W // 2, H - 30))
+
+
+def dialogs(text, pos, owl_text):
+    screen.blit(dialog, pos)
+    screen.blit(font2.render(text, True, BLACK), (pos[0] + 5, pos[1] + 5))
+    screen.blit(dialog, dialog_owl_pos)
+    screen.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, dialog_owl_pos[1] + 5))
+    pygame.display.update()
+    pygame.time.wait(2000)
+
 
 run = True
 while run:
@@ -34,8 +58,11 @@ while run:
             if e.key == pygame.K_ESCAPE:
                 run = False
 
-    screen.blit(bg, bg_rect)
-    screen.blit(cat, cat_rect)
-    screen.blit(dog, dog_rect)
-    screen.blit(owl, owl_rect)
+    if block == 0:
+        screen.blit(bg, bg_rect)
+        screen.blit(cat, cat_rect)
+        screen.blit(dog, dog_rect)
+        screen.blit(owl, owl_rect)
+        screen.blit(font_box, font_box_rect)
+        font_box.fill(SILVER)
     pygame.display.update()
---
+++ test.py
@@ -0,0 +1,6 @@
+def f(a, b, c):
+    d = a + b * c
+    print(d)
+
+
+f(2, 10, 5)
